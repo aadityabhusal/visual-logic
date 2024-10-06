3e8bfeba1547c712b734e4da2ad774f893cb9c75
aadityabhusal <bhusal.001aditya@gmail.com>
2024-10-06 23:59:35 +0545

    chore: removes styled components and installs tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fafafa",
+        reserved: "#569cd6",
+        variable: "#4fc1ff",
+        type: "#4ec9b0",
+        property: "#9cdcfe",
+        string: "#ce9178",
+        number: "#b5cea8",
+        boolean: "#569cd6",
+        array: "#fafafa",
+        object: "#fafafa",
+        method: "#dcdcaa",
+        border: "#444444",
+        disabled: "#7f8c8d",
+        error: "#c7254e",
+      },
+      backgroundColor: {
+        editor: "#1e1e1e",
+        dropdown: {
+          default: "#252526",
+          hover: "#2a2d2e",
+          scrollbar: "#464647",
+          selected: "#04395e",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
